cee20bf7c7b20da3deab1ca2b2e4b85f34c1e347
jie_ke11 <2191982744@qq.com>
2025-03-19 13:56:09 +0800

    fix: fatherrc

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,9 +1,3 @@
-// import { defineConfig } from 'father';
-
-// export default defineConfig({
-//   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
-//   esm: { output: 'dist' },
-// });
 import { defineConfig } from 'father';
 
 export default defineConfig({
